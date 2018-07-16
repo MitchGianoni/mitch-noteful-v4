e71714aee3426ff6f7a21b59d650d69d52de970d
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-16 16:10:21 -0400

    added login route and fixed local passport module

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const User = require('../models/user');
+const router = express.Router();
+
+const options = { session: false, failWithError: true};
+const localAuth = passport.authenticate('local', options);
+
+// POST protected login route
+router.post('/login', localAuth, function(req, res) {
+  return res.json(req.user);
+});
+
+module.exports = router;
